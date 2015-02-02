7afbd01495db8b354a7cca885831d0d4f259c198
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2015-02-02 21:24:49 +0100

    Added more sockets

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var app     = express();
-var server  = require('http').createServer(app);
-var io      = require('socket.io').listen(server);
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 var port = process.env.PORT || 8080;
 var _ = require('lodash');
 
-var currentContinentData = {
-    total : 0,
-    continents: {
+var currentPoopinCount = {
+    "total": 0,
+    "continents": {
         "Europe": 0,
         "N. America": 0,
         "S. America": 0,
@@ -17,49 +17,83 @@ var currentContinentData = {
     }
 };
 
-app.get('/', function(req, res){
+var socketsPerContinent = {
+    "Europe": [],
+    "N. America": [],
+    "S. America": [],
+    "Asia": [],
+    "Africa": [],
+    "Australia": []
+};
+
+app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-function randomNumber() {
-    return Math.ceil((Math.random() * 1000 ) + 1)
-}
-
-function registerPooper(data) {
-    console.log(data)
-
-    currentContinentData.continents[data["continent"]]++;
-    var total = currentContinentData.total;
-
-    _.each(currentContinentData.continents, function(value, key) {
-        total = total + value
+function recalculateTotals() {
+    _.each(socketsPerContinent, function (value, continent) {
+        //console.log("Value " + value + " for key " + key);
+        //console.log("SOCKETS " + socketsPerContinent);
+        currentPoopinCount.continents[continent] = value.length;
     });
 }
 
-function send() {
-    //var data = {
-    //    total : "",
-    //    continents: {
-    //        "Europe": randomNumber(),
-    //        "N. America": randomNumber(),
-    //        "S. America": randomNumber(),
-    //        "Asia": randomNumber(),
-    //        "Africa": randomNumber(),
-    //        "Australia": randomNumber()
-    //    }
-    //};
-
-    io.emit("updatePoopinStats", currentContinentData)
+function recalculateTotalsFor(continent) {
+    currentPoopinCount.continents[continent] = socketsPerContinent[continent].length;
 }
 
-setInterval(function(){ send(); }, 3000);
+function recalculateTotal() {
+    var total = 0;
+
+    _.each(currentPoopinCount.continents, function (value, key) {
+        total = total + value;
+    });
+
+    currentPoopinCount.total = total
+}
+
+function registerPooper(data, mySocket) {
+    var continent = data["continent"];
+    var socketUid = mySocket.id;
+
+    socketsPerContinent[continent].push(socketUid);
+    console.log(socketsPerContinent[continent])
+    //socketsPerContinent = _.uniq(socketsPerContinent);
+
+    recalculateTotals();
+    recalculateTotal();
+}
+
+function deRegisterPooper(data, mySocket) {
+    var continent = data["continent"];
+    var socketUid = mySocket.id;
+
+    _.remove(socketsPerContinent[continent], function(n) {return n === socketUid;});
+
+    recalculateTotalsFor(continent);
+    recalculateTotal();
+}
+
+function send() {
+    io.emit("updatePoopinStats", currentPoopinCount)
+}
+
+setInterval(function () {
+    send();
+}, 3000);
 
 io.on('connection', function (socket) {
-    console.log("connection");
+    //var mySocket = socket;
+    console.log(socket.id);
 
     socket.on('initialConnection', function (data) {
-        registerPooper(data);
-        setInterval(function(){ send(); }, 3000);
+        console.log("initial connection");
+        registerPooper(data, socket);
+    });
+
+    socket.on('disconnect', function (data) {
+        console.log("connection lost");
+        deRegisterPooper({"continent" : "Europe"}, socket)
     });
 });
 
